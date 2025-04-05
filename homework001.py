ac12d72d16b545cff7ade6865c362e97112dd503
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2025-04-05 19:15:14 +0500

    Add files via upload

---
+++ homework001.py
@@ -0,0 +1,9 @@
+import math
+a=float(input("Enter the angle in degree: "))
+b=math.radians(a)
+sin=math.sin(b)
+cos=math.cos(b)
+tan=math.tan(b)
+print(f"Sin: {sin}")
+print(f"Cos: {cos}")
+print(f"Tan: {tan}")
